ea2b0b7d41e4ab41f8e3a6039f038a4919f755ac
Diana Pavel <dianapav@gmail.com>
2020-01-13 17:46:28 +0000

    added mentees and mentors tables

--- code/diana-pavel/didi/db/migrate/20200113162103_create_mentees.rb
+++ code/diana-pavel/didi/db/migrate/20200113162103_create_mentees.rb
@@ -1,6 +1,10 @@
 class CreateMentees < ActiveRecord::Migration[5.2]
   def change
     create_table :mentees do |t|
+      t.string :name
+      t.string :email
+      t.string :job_title
+
     end
   end
 end
--- code/diana-pavel/didi/db/migrate/20200113162605_create_mentors.rb
+++ code/diana-pavel/didi/db/migrate/20200113162605_create_mentors.rb
@@ -1,6 +1,14 @@
 class CreateMentors < ActiveRecord::Migration[5.2]
   def change
     create_table :mentors do |t|
+      t.string :name
+      t.string :job_title
+      t.string :current_company
+      t.string :github
+      t.string :personal_website
+      t.string :linkedin
+      t.integer :hours
+      t.boolean :content
     end
   end
 end
